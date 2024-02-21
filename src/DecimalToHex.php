9ef9bc44130f8df2f442a591735932885a48e7de
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-02-21 23:09:50 +0330

    HexToDecimal function created

--- src/DecimalToHex.php
+++ src/DecimalToHex.php
@@ -18,4 +18,19 @@ class DecimalToHex
 
         return str_pad($hex, 4, "0", STR_PAD_LEFT);
     }
+
+    public static function HexToDecimal(string $hex): int {
+        $result = 0;
+        $length = strlen($hex);
+        $hex = strtoupper($hex);
+
+        foreach (str_split($hex) as $key => $value) {
+            $index = $key+1;
+            $number = array_search($value, str_split(self::DIGITS));
+            $pow = $length - $index;
+            $result += $number * (16**$pow);
+        }
+
+        return $result;
+    }
 }
