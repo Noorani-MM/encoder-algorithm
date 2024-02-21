6602304cdca9b5dad21dc76553a78e7c6c8ce13d
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-02-21 23:59:37 +0330

    Encoder bug fix

--- src/Coder.php
+++ src/Coder.php
@@ -30,7 +30,7 @@ class Coder
             $result .= chr($c_ord);
         }
 
-        return implode("", [$keys[0], $keys[1], $hex, $result, $length, $checks['key1'][0], $checks['key2'][0], $checks['key1'][1], $checks['key2'][1]]);
+        return implode("", [chr($keys[0]), chr($keys[1]), $hex, $result, $length, $checks['key1'][0], $checks['key2'][0], $checks['key1'][1], $checks['key2'][1]]);
     }
 
     protected static function keys(): array
