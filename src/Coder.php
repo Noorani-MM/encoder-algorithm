d401a8ec28f810aea52370ed766ec71ee2130439
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-02-22 14:38:35 +0330

    Encoder upgraded and decoder added

--- src/Coder.php
+++ src/Coder.php
@@ -2,56 +2,92 @@
 
 namespace NooraniMm\EncoderAlgorithm;
 
+use NooraniMm\EncoderAlgorithm\Exceptions\InvalidEncodeException;
+use NooraniMm\EncoderAlgorithm\Exceptions\InvalidKeyException;
+
 class Coder
 {
+    /**
+     * @throws InvalidKeyException
+     */
     public static function encode(string $value): string
     {
         $result = "";
         $content = new Content($value);
+        $value = str_replace(' ', '♂', $value);
 
         $hex = DecimalToHex::DecimalToHex($content->sum());
         $length = DecimalToHex::DecimalToHex($content->length());
 
-        $keys = self::keys();
-        $key_ords = [ord($keys[0]), ord($keys[1])];
-
-        $checks = self::key_check($keys);
+        $key1 = new Key(rand(1, 26) + 64);
+        $key2 = new Key(rand(1, 26) + 64);
 
         foreach (str_split($value) as $index => $char) {
             $c_ord = ord($char);
             // Code with key1
             if ($index % 2 === 0) {
-                $c_ord += ($key_ords[0] - 64);
+                $c_ord += ($key1->code());
             }
             // Code with key2
             else {
-                $c_ord -= ($key_ords[1] - 64);
+                $c_ord -= ($key2->code());
             }
             $result .= chr($c_ord);
         }
 
-        return implode("", [chr($keys[0]), chr($keys[1]), $hex, $result, $length, $checks['key1'][0], $checks['key2'][0], $checks['key1'][1], $checks['key2'][1]]);
+        return implode("", [$key1->character(), $key2->character(),
+            $hex, $result, $length,
+            $key1->validation()[0], $key2->validation()[0], $key1->validation()[1], $key2->validation()[1]]);
     }
 
-    protected static function keys(): array
+    /**
+     * @throws InvalidKeyException
+     * @throws InvalidEncodeException
+     */
+    public static function decode(string $encode)
     {
-        $key1 = rand(1, 26) + 64;
-        $key2 = rand(1, 26) + 64;
+        $length = strlen($encode);
+        $key1 = Key::generator($encode[0], [$encode[$length - 4], $encode[$length - 2]]);
+        $key2 = Key::generator($encode[1], [$encode[$length - 3], $encode[$length - 1]]);
+        $encontent = self::get_encoded_content($encode);
+        $en_result = self::get_encoded_result($encode);
+        $en_length = self::get_encoded_length($encode);
 
-        return [$key1, $key2];
+        $result = '';
+        foreach (str_split($encontent) as $index => $char) {
+            $ord = ord($char);
+            if ($index % 2 === 0) {
+                $ord -= $key1->code();
+            }
+            else {
+                $ord += $key2->code();
+            }
+            $result .= chr($ord);
+        }
+
+        $result = str_replace('♂', ' ', $result);
+        $content = new Content($result);
+        if (($content->sum() !== DecimalToHex::HexToDecimal($en_result)) || ($content->length() !== DecimalToHex::HexToDecimal($en_length))) {
+            throw new InvalidEncodeException("This encode is invalid");
+        }
+
+        return $result;
     }
 
-    protected static function key_check(array $keys): array
+    protected static function get_encoded_content(string $encode): string
     {
-        $code1 = $keys[0] - 64;
-        $code2 = $keys[1] - 64;
+        $length = strlen($encode);
+        return substr($encode, 6, ($length - 14));
+    }
 
-        $chk11 = chr(($code1 / 2) + 64);
-        $chk12 = chr(($code1 % 2) + 65);
+    protected static function get_encoded_result(string $encode): string
+    {
+        return substr($encode, 2, 4);
+    }
 
-        $chk21 = chr(($code2 / 2) + 64);
-        $chk22 = chr(($code2 % 2) + 65);
-
-        return ['key1' => [$chk11, $chk12], 'key2' => [$chk21, $chk22]];
+    protected static function get_encoded_length(string $encode): string
+    {
+        $length = strlen($encode);
+        return substr($encode, $length - 8, 4);
     }
 }
---
+++ src/Exceptions/InvalidEncodeException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace NooraniMm\EncoderAlgorithm\Exceptions;
+
+class InvalidEncodeException extends \Exception
+{
+
+}
