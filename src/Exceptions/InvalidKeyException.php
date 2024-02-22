1a774092ef1e070d5734b5b8c4d9cefc0f691a25
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-02-22 14:26:02 +0330

    InvalidKeyException created

---
+++ src/Exceptions/InvalidKeyException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace NooraniMm\EncoderAlgorithm\Exceptions;
+
+class InvalidKeyException extends \Exception
+{
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
