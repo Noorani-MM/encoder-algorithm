7e13021068b07d13c632d2527694b8f052fa22a6
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-03-29 03:06:54 +0330

    update Char class and remove Binary

--- src/Char.php
+++ src/Char.php
@@ -6,17 +6,19 @@ use NooraniMm\EncoderAlgorithm\Exceptions\InvalidCharException;
 
 class Char
 {
+    private array $bytes;
     public function __construct(protected string $char)
     {
-    }
-
-    public function ascii(): int
-    {
-        return ord($this->char);
+        $this->char = mb_convert_encoding($this->char, 'UTF-8', 'UTF-8');
+        $this->bytes = unpack('C*', $this->char);
     }
 
     public function binary(): string
     {
-        return Binary::int_to_binary($this->ascii());
+        $binary = '';
+        foreach ($this->bytes as $byte) {
+            $binary .= str_pad(decbin($byte), 8, '0', STR_PAD_LEFT);
+        }
+        return $binary;
     }
 }
