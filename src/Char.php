2019ddf3699bdd8a6cb8cb30120a9b70e20ded3a
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-03-29 05:22:27 +0330

    Update char class

--- src/Char.php
+++ src/Char.php
@@ -2,29 +2,15 @@
 
 namespace NooraniMm\EncoderAlgorithm;
 
-use NooraniMm\EncoderAlgorithm\Exceptions\InvalidCharException;
-
 class Char
 {
-    private array $bytes;
     public function __construct(protected string $char)
     {
-        $this->char = mb_convert_encoding($this->char, 'UTF-8', 'UTF-8');
-        $this->bytes = unpack('C*', $this->char);
     }
 
     public function binary(): string
     {
-        $binary = '';
-        foreach ($this->bytes as $byte) {
-            $binary .= str_pad(decbin($byte), 8, '0', STR_PAD_LEFT);
-        }
-        return $binary;
-    }
-
-    public function bytes(): false|array
-    {
-        return $this->bytes;
+        return str_pad(base_convert(ord($this->char), 10, 2),8, '0', STR_PAD_LEFT);
     }
 
     public function char(): string
@@ -32,13 +18,12 @@ class Char
         return $this->char;
     }
 
-    public static function char_by_binary(string $binary): Char
+    public static function char_array(array $chars): array
     {
-        $segments = str_split($binary, 8);
-        $chars = '';
-        foreach ($segments as $segment) {
-            $chars .= pack('H*', base_convert($segment, 2, 16));
+        $data= [];
+        foreach ($chars as $char) {
+            $data[] = new self($char);
         }
-        return new self($chars);
+        return $data;
     }
 }
